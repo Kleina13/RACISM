40a0c7ee101b4e6ec729399144f137417f319cdb
Kleina13 <ragnarhelgi22@gmail.com>
2019-09-03 22:39:28 +0000

    klarað

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask as poop
-app = poop(__name__)
+# APP
+
+from flask import Flask, render_template as rend
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Halló heimur</h1>'
+	return rend('template.html') + '<h1>Hello World!</h1>'
+
+@app.route('/info/')
+def info():
+	return rend('template.html') + '<h1>This is the info page</h1>'
+
+@app.route('/apps/')
+def apps():
+	return rend('template.html') + '<h1>this is the apps page</h1>'
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
